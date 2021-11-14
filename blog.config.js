b4946cfea9c9c2ee63e5ea4396e6f06408e9efaf
daisukesone <sonedai0213@gmail.com>
2021-11-14 21:22:38 +0900

    change to en-us

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: "sonedai0213@gmail.com",
   link: "https://www.daisukesone.me/",
   description: "It's a piece of cake.",
-  lang: "ja-JP", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: "auto", // ['light', 'dark', 'auto'],
   font: "sans-serif", // ['sans-serif', 'serif']
   lightBackground: "#ffffff", // use hex value, don't forget '#' e.g #fffefc
