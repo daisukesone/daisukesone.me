e7c0ca6fd0166b237b4990fc851b8ac01651f3c0
daisukesone <sonedai0213@gmail.com>
2021-11-14 21:06:19 +0900

    update my profile

--- blog.config.js
+++ blog.config.js
@@ -2,9 +2,9 @@ const BLOG = {
   title: "daisukesone.me",
   author: "daisukesone",
   email: "sonedai0213@gmail.com",
-  link: "https://nobelium.vercel.app",
-  description: "This gonna be an awesome website.",
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  link: "https://www.daisukesone.me/",
+  description: "It's a piece of cake.",
+  lang: "ja-JP", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: "auto", // ['light', 'dark', 'auto'],
   font: "sans-serif", // ['sans-serif', 'serif']
   lightBackground: "#ffffff", // use hex value, don't forget '#' e.g #fffefc
@@ -12,12 +12,12 @@ const BLOG = {
   path: "", // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 11,
-  sortByDate: false,
+  sortByDate: true,
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: "https://og-image-craigary.vercel.app", // The link to generate OG image, don't end with a slash
-  socialLink: "",
+  socialLink: "https://twitter.com/daisukesone213",
   seo: {
     keywords: ["Blog", "Website", "Notion"],
     googleSiteVerification: "", // Remove the value or replace it with your own google site verification code
